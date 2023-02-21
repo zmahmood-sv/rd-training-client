18ec0ab8355b277e516c77791fbd0b0b0991d83f
zmahmood-sv <zeshan.mahmood@simpleviewinc.com>
2023-02-21 10:29:53 +0000

    silly fix

--- src/prefixes/TrainingPrefix.js
+++ src/prefixes/TrainingPrefix.js
@@ -2,7 +2,7 @@ const {Movies} = require('./collections/Movies');
 const {People} = require('./collections/People');
 
 class TrainingPrefix {
-    constructor(graphUrl, graphServer) {
+constructor({graphUrl, graphServer}) {
         this.name = "training";
         this._graphUrl = graphUrl;
         this._graphServer = graphServer;
