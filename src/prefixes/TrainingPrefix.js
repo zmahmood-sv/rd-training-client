d6e55ced4a84792787d35d21cfa2fd709935e8f4
zmahmood-sv <zeshan.mahmood@simpleviewinc.com>
2023-02-28 08:34:56 +0000

    added test functions

--- src/prefixes/TrainingPrefix.js
+++ src/prefixes/TrainingPrefix.js
@@ -1,14 +1,46 @@
-const {Movies} = require('./collections/Movies');
-const {People} = require('./collections/People');
+const { Movies } = require('./collections/Movies');
+const { People } = require('./collections/People');
 
 class TrainingPrefix {
-constructor({graphUrl, graphServer}) {
+    constructor({ graphUrl, graphServer }) {
         this.name = "training";
         this._graphUrl = graphUrl;
         this._graphServer = graphServer;
-        this.movies = new Movies({graphUrl,graphServer});
-        this.people = new People({graphUrl,graphServer});
+        this.movies = new Movies({ graphUrl, graphServer });
+        this.people = new People({ graphUrl, graphServer });
+    }
+
+    async training_test_data_entry() {
+        return await query({
+            query: `
+				mutation{
+					training {
+						tests_setup {
+							success,
+							message
+						}
+					}
+				}
+			`,
+            url: this._graphUrl
+        });
+    }
+
+    async training_test_data_removal() {
+        return await query({
+            query: `
+				mutation {
+					training {
+						tests_clear {
+							success,
+							message
+						}
+					}
+				}
+			`,
+            url: this._graphUrl
+        });
     }
 }
 
-module.exports = {TrainingPrefix};
+module.exports = { TrainingPrefix };
