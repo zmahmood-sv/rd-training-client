a3426b7975d76124477d4f1f71749ba5d275a783
zmahmood-sv <zeshan.mahmood@simpleviewinc.com>
2023-03-22 15:30:01 +0000

    indentation :)

--- src/prefixes/TrainingPrefix.js
+++ src/prefixes/TrainingPrefix.js
@@ -2,17 +2,17 @@ const { Movies } = require("./collections/Movies");
 const { People } = require("./collections/People");
 const { query } = require("@simpleview/sv-graphql-client");
 class TrainingPrefix {
-    constructor({ graphUrl, graphServer }) {
-        this.name = "training";
-        this._graphUrl = graphUrl;
-        this._graphServer = graphServer;
-        this.movies = new Movies({ graphUrl, graphServer });
-        this.people = new People({ graphUrl, graphServer });
-    }
+	constructor({ graphUrl, graphServer }) {
+		this.name = "training";
+		this._graphUrl = graphUrl;
+		this._graphServer = graphServer;
+		this.movies = new Movies({ graphUrl, graphServer });
+		this.people = new People({ graphUrl, graphServer });
+	}
 
-    async training_test_data_entry() {
-        return await query({
-            query: `
+	async training_test_data_entry() {
+		return await query({
+			query: `
 				mutation{
 					training {
 						training_test_data_entry {
@@ -22,13 +22,13 @@ class TrainingPrefix {
 					}
 				}
 			`,
-            url: this._graphUrl
-        });
-    }
+			url: this._graphUrl
+		});
+	}
 
-    async training_test_data_removal() {
-        return await query({
-            query: `
+	async training_test_data_removal() {
+		return await query({
+			query: `
 				mutation {
 					training {
 						training_test_data_removal {
@@ -38,9 +38,9 @@ class TrainingPrefix {
 					}
 				}
 			`,
-            url: this._graphUrl
-        });
-    }
+			url: this._graphUrl
+		});
+	}
 }
 
 module.exports = { TrainingPrefix };
--- src/prefixes/collections/Movies.js
+++ src/prefixes/collections/Movies.js
@@ -1,84 +1,83 @@
 const { query } = require("@simpleview/sv-graphql-client");
 
 class Movies {
-  constructor({ graphUrl, graphServer }) {
-    this.name = "Movies";
-    this._graphurl = graphUrl;
-    this._graphServer = graphServer;
-  }
+	constructor({ graphUrl, graphServer }) {
+		this.name = "Movies";
+		this._graphurl = graphUrl;
+		this._graphServer = graphServer;
+	}
 
 
-  async find({ input, context, fields, headers }) {
+	async find({ input, context, fields, headers }) {
 
-    return await query({
-      query: `
-            query Docs ($param:training_movies_find_param){
-                training{
-                  movies_find(param:$param){
-                   ${fields}
-                  }
-                }
-              }
-              
-            `,
-      variables: {
-        param: input
-      },
-      url: this._graphurl,
-      headers,
-      clean: true,
-      key: "training.movies_find",
-
-
-    });
-  }
+		return await query({
+			query: `
+					query Docs ($param:training_movies_find_param){
+						training{
+							movies_find(param:$param){
+				 				${fields}
+							}
+						}
+					}
+			`,
+			variables: {
+				param: input
+			},
+			url: this._graphurl,
+			headers,
+			clean: true,
+			key: "training.movies_find",
 
 
-  async remove({ input, context, fields, headers }) {
+		});
+	}
 
-    return await query({
-      query: `
-            mutation training($param:training_movies_remove_input){
-              training{
-                movies_remove(input:$param){
-                  ${fields}
-                }
-              }
-            }	
-            `,
-      variables: {
-        param: input
-      },
-      url: this._graphurl,
-      headers,
-      clean: true,
-      key: "training.movies_remove",
-    });
 
-  }
+	async remove({ input, context, fields, headers }) {
 
-  async insert({ input, context, fields, headers }) {
+		return await query({
+			query: `
+				mutation training($param:training_movies_remove_input){
+					training{
+						movies_remove(input:$param){
+							${fields}
+						}
+					}
+				}	
+			`,
+			variables: {
+				param: input
+			},
+			url: this._graphurl,
+			headers,
+			clean: true,
+			key: "training.movies_remove",
+		});
 
-    return await query({
-      query: `
-            mutation training($param:[training_movies_insert!]!){
-                training{
-                  movies_insert(movies:$param){
-                    ${fields}
-                  }
-                }
-              }	
-            `,
-      variables: {
-        param: input
-      },
-      url: this._graphurl,
-      headers,
-      clean: true,
-      key: "training.movies_insert",
-    });
+	}
 
-  }
+	async insert({ input, context, fields, headers }) {
+
+		return await query({
+			query: `
+				mutation training($param:[training_movies_insert!]!){
+					training{
+						movies_insert(movies:$param){
+							${fields}
+						}
+					}
+				}	
+			`,
+			variables: {
+				param: input
+			},
+			url: this._graphurl,
+			headers,
+			clean: true,
+			key: "training.movies_insert",
+		});
+
+	}
 }
 
 module.exports = { Movies };
