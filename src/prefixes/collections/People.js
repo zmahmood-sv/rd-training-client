fe29e05842533941d3a5fe1462b359194ade68ca
zmahmood-sv <121865147+zmahmood-sv@users.noreply.github.com>
2023-04-05 13:36:24 +0100

    Update People.js

--- src/prefixes/collections/People.js
+++ src/prefixes/collections/People.js
@@ -1,80 +1,80 @@
 const { query } = require("@simpleview/sv-graphql-client");
 class People {
-    constructor({ graphUrl, graphServer }) {
-        this.name = "People";
-        this._graphurl = graphUrl;
-        this._graphServer = graphServer;
-    }
+	constructor({ graphUrl, graphServer }) {
+		this.name = "People";
+		this._graphurl = graphUrl;
+		this._graphServer = graphServer;
+	}
 
-    async find({ input, context, fields, headers }) {
+	async find({ input, fields, headers }) {
 
-        return await query({
-            query: `
-            query training($input:training_people_find_param){
-                training{
-                      people_find(input:$input){
-                        ${fields}
-                    }
-                }
-            }`,
-            variables: {
-             input
-            },
-            url: this._graphurl,
-            headers,
-            clean: true,
-            key: "training.people_find",
+		return await query({
+			query: `
+			query training($findId:training_people_find_input){
+				training{
+						people_find(param:$findId){
+						${ fields }
+					}
+				}
+			}`,
+			variables: {
+				param: input
+			},
+			url: this._graphurl,
+			headers,
+			clean: true,
+			key: "training.people_find",
 
-        });
+		});
 
-    }
+	}
 
-    async remove({ input, context, fields, headers }) {
+	async remove({ input, fields, headers }) {
 
-        return await query({
-            query: `
-            mutation training($input:training_people_remove_input){
-                training{
-                      people_remove(input:$input){
-                          ${fields}
-                      }
-                  }
-              }	
-            `,
-            variables: {
-                input
-            },
-            url: this._graphurl,
-            headers,
-            clean: true,
-            key: "training.people_remove",
+		return await query({
+			query: `
+			mutation training($param:training_people_remove_input){
+				training{
+					people_remove(input:$param){
+						${ fields }
+					}
+				}
+			}	
+			`,
+			variables: {
+				param: input
+			},
+			url: this._graphurl,
+			headers,
+			clean: true,
+			key: "training.people_remove",
 
-        });
+		});
 
-    }
+	}
 
-    async insert({ input, context, fields, headers }) {
+	async insert({ input, fields, headers }) {
 
-        return await query({
-            query: `
-            mutation Insert($input: [training_people_insert!]!){
-                training {
-                  people_insert(input: $input){
-                    ${fields}   
-                  }
-                }
-              }
-            `,
-            variables: {
-             input
-            },
-            url: this._graphurl,
-            headers,
-            clean: true,
-            key: "training.people_insert",
+		return await query({
+			query: `
+			mutation Insert($people: [training_people_insert_input!]!){
+				training {
+					people_insert(people: $people){
+						${ fields }
+					}
+				}
+			}
+			`,
+			variables: {
+				param: input
+			},
+			url: this._graphurl,
+			headers,
+			clean: true,
+			key: "training.people_insert",
 
-        });
-    }
+		});
+	}
 }
 
 module.exports = { People };
